80ba2d66146424f9c5b95cb2adc4d79a773818fb
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2011-02-27 14:49:58 -0500

    Basic list of spreadsheets for the foloowing flow:
    Input the email/password for google docs:
      /users/
    
    Redirect to /spreadsheets/ to view spreadsheets.

--- urls.py
+++ urls.py
@@ -6,12 +6,18 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^user/$', 'count.views.user'),
+    (r'^spreadsheets/$', 'count.views.spreadsheets'),
+    (r'^count/$', 'count.views.index'),
+    (r'^count/(?P<count_id>\d+)/$', 'count.views.detail'),
+    (r'^count/(?P<count_id>\d+)/results/$', 'count.views.results'),
+    (r'^count/(?P<count_id>\d+)/vote/$', 'count.views.vote'),
+    (r'^admin/', include(admin.site.urls)),
     # Example:
     # (r'^owords/', include('owords.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
 )
